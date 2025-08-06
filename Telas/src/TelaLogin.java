4067d20a45d596b0565fc40574c7f9fd7c9643be
Henrique Conradt <henriqueconradt@gmail.com>
2025-08-06 10:42:18 -0300

    Tela de login v1

---
+++ Telas/src/TelaLogin.java
@@ -0,0 +1,113 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Canvas;
+import java.awt.Color;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SwingConstants;
+import java.awt.Panel;
+import java.awt.Label;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+
+public class TelaLogin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 788, 560);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setForeground(Color.WHITE);
+		btnLogin.setBackground(Color.BLUE);
+		btnLogin.setBounds(324, 346, 136, 33);
+		contentPane.add(btnLogin);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(324, 202, 136, 33);
+		txtUsuario.setFocusable(true);
+		
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(81, 5, 623, 59);
+		panel.setBackground(Color.BLUE);
+		panel.setForeground(Color.BLUE);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("WorkITBr");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblNewLabel.setBounds(41, 0, 543, 64);
+		panel.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBackground(Color.BLUE);
+		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtUsuario.setForeground(Color.BLACK);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblUsuario = new JLabel("Usuário");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setBounds(324, 164, 136, 27);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(324, 263, 136, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(324, 288, 136, 33);
+		contentPane.add(passwordField);
+		
+
+		
+		
+		
+	}
+}
