d75a7f842e98ce058b7519fc4f722b2c37e3c8e4
Prange <prange.vitor@gmail.com>
2025-08-11 12:09:50 -0300

    TelaComRedimensionamentoSemiPtonto
    tela com redimensionamento +- funcional

--- Telas_PI/src/packageDefault/TelaLogin.java
+++ Telas_PI/src/packageDefault/TelaLogin.java
@@ -1,11 +1,20 @@
 package packageDefault;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
+
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JComboBox;
+import java.awt.FlowLayout;
 
 public class TelaLogin extends JFrame {
 
@@ -32,18 +41,41 @@ public class TelaLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaLogin() {
-		setResizable(false);
+		setTitle("WorkITBr");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1473, 794);
+		setBounds(100, 100, 1169, 788);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new MigLayout("fill, insets 0", "[grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow]", 
+				"[35px][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow]"));
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.BLUE);
-		panel.setBounds(0, 0, 1457, 62);
-		contentPane.add(panel);
+		contentPane.add(panel, "cell 0 0 24 2,grow");
+		panel.setLayout(new MigLayout("fill", "[center]", "[]"));
+		
+		JLabel lblNewLabel = new JLabel("WorkITBr");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel.setForeground(Color.WHITE);
+		panel.add(lblNewLabel, "cell 0 0, grow, push");
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		contentPane.add(panel_1, "cell 2 6 2 1");
+		
+		panel.addComponentListener(new ComponentAdapter() {
+		    @Override
+		    public void componentResized(ComponentEvent e) {
+		        int panelHeight = panel.getHeight();
+		        int fontSize = Math.max(35, panelHeight / 3);
+		        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, fontSize));
+		    }
+		});
+		
+		
 	}
 }
