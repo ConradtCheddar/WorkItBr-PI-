e9416138a79ea2fd4104ce42446de02425e21296
Prange <prange.vitor@gmail.com>
2025-08-18 11:54:49 -0300

    Telas_Final v3
    Adicionado label Senha

---
+++ Telas_Final/src/telas_Final/TelaCadastro1.java
@@ -0,0 +1,19 @@
+package telas_Final;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+public class TelaCadastro1 extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public TelaCadastro1() {
+		setPreferredSize(new Dimension(984, 736));
+		
+
+	}
+
+}
