1e1ea45502fdfeed5785c73f6363b966d54d3da9
Gustavo Grocelli Rosa <rosaguga07@gmail.com>
2025-08-27 11:55:51 -0300

    script milagroso

--- coisas do mysql/Script MySql.sql
+++ coisas do mysql/Script MySql.sql
@@ -2,13 +2,6 @@ CREATE Database if not exists WorkItBr_BD;
 
 use WorkItBr_BD;
 
-CREATE TABLE if not exists Contratado 
-( 
- Github INT,  
- idLogin INT,  
- idContratado boolean PRIMARY KEY 
-); 
-
 CREATE TABLE if not exists Servico 
 ( 
  ID_servico INT PRIMARY KEY AUTO_INCREMENT,  
@@ -17,12 +10,6 @@ CREATE TABLE if not exists Servico
  Descricao VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE if not exists Contratante 
-( 
- id_contratante boolean PRIMARY KEY,  
- idLogin INT
-); 
-
 CREATE TABLE if not exists Login 
 ( 
  Email VARCHAR(25) NOT NULL DEFAULT '20',  
@@ -30,23 +17,22 @@ CREATE TABLE if not exists Login
  CPF_CNPJ varchar(12) NOT NULL,  
  Telefone VARCHAR(12) NOT NULL, 
  Senha VARCHAR(50) NOT NULL, 
- idLogin INT PRIMARY KEY AUTO_INCREMENT  
+ github VARCHAR(50),
+ idLogin INT PRIMARY KEY AUTO_INCREMENT, 
+ idContratado boolean,
+ idAdmin boolean,
+ idContratante boolean
 ); 
 
-CREATE TABLE if not exists administrador 
-( 
- id_admin boolean PRIMARY KEY,  
- idLogin INT 
-); 
-
-ALTER TABLE  Contratado ADD FOREIGN KEY(idLogin) REFERENCES Login (idLogin);
-ALTER TABLE Contratante ADD FOREIGN KEY(idLogin) REFERENCES Login (idLogin);
-ALTER TABLE administrador ADD FOREIGN KEY(idLogin) REFERENCES Login (idLogin);
-
-INSERT INTO login (Nome, Email, CPF_CNPJ, Telefone, Senha, id_admin)
+INSERT INTO login (Nome, Email, CPF_CNPJ, Telefone, Senha, idAdmin)
 SELECT 'Admin', 'default', 'default', 'default', 'workitbr@321', true
 WHERE NOT EXISTS (
 SELECT 1 FROM login WHERE Nome = 'Admin');
 
 
 select * from Login
+
+
+
+
+
