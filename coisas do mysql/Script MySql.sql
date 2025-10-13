c88fb5962dba583650be5de2b58f2e478f168439
ConradtCheddar <henriqueconradt@gmail.com>
2025-10-13 12:08:31 -0300

    melhoria banco

--- coisas do mysql/Script MySql.sql
+++ coisas do mysql/Script MySql.sql
@@ -3,16 +3,6 @@ CREATE Database if not exists WorkItBr_BD;
 
 use WorkItBr_BD;
 
-CREATE TABLE if not exists Servico 
-( 
- ID_servico INT PRIMARY KEY AUTO_INCREMENT, 
- Nome_servico varchar(25) not null,
- Valor int NOT NULL,  
- Modalidade VARCHAR(50) NOT NULL,  
- Descricao longtext NOT NULL,
- Aceito boolean
-);
-
 CREATE TABLE if not exists Login 
 ( 
  Email VARCHAR(60) NOT NULL DEFAULT '20',  
@@ -22,19 +12,37 @@ CREATE TABLE if not exists Login
  Senha VARCHAR(60) NOT NULL, 
  github VARCHAR(60),
  idLogin INT PRIMARY KEY AUTO_INCREMENT, 
- idContratado boolean,
- idAdmin boolean,
- idContratante boolean,
+ isContratado boolean,
+ isAdmin boolean,
+ isContratante boolean,
  caminhofoto VARCHAR(255)
 ); 
+
+CREATE TABLE if not exists Servico 
+( 
+ ID_servico INT PRIMARY KEY AUTO_INCREMENT, 
+ Nome_servico varchar(25) not null,
+ Valor int NOT NULL,  
+ Modalidade VARCHAR(50) NOT NULL,  
+ Descricao longtext NOT NULL,
+ Aceito boolean,
+ `id_contratado` int DEFAULT NULL,
+  `id_contratante` int NOT NULL,
+  KEY `fk_idContratante_idx` (`id_contratante`),
+  KEY `fk_idContratado_idx` (`id_contratado`),
+  CONSTRAINT `fk_idContratado_login` FOREIGN KEY (`id_contratado`) REFERENCES `login` (`idLogin`),
+  CONSTRAINT `fk_idContratante_login` FOREIGN KEY (`id_contratante`) REFERENCES `login` (`idLogin`)
+
+);
+
 
 INSERT INTO login (Nome_Usuario, Email, CPF_CNPJ, Telefone, Senha, idAdmin)
 SELECT 'Admin', 'default', 'default', 'default', 'workitbr@321', true
 WHERE NOT EXISTS (
 SELECT 1 FROM login WHERE Nome_Usuario = 'Admin');
 
-INSERT INTO servico (Nome_servico, Valor, Modalidade, Descricao, Aceito)
-SELECT 'teste1', '100', 'default', 'default', false
+INSERT INTO servico (Nome_servico, Valor, Modalidade, Descricao, Aceito, id_contratante)
+SELECT 'teste1', '100', 'default', 'default', false, FALSE
 WHERE NOT EXISTS (
 SELECT 1 FROM servico  WHERE Nome_servico = 'teste1');
 
